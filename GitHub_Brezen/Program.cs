a037334e311c3f38f99f1e8009fddcd39f88caba
Daniel Duda <daniel.duda@educhem.cz>
2022-03-21 11:30:20 +0100

    Přidejte soubory projektu.

---
+++ GitHub_Brezen/Program.cs
@@ -0,0 +1,4 @@
+﻿int cislo1 = Convert.ToInt32(Console.ReadLine());
+int cislo2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{Math.Max(cislo1, cislo2)}, {Math.Min(cislo1, cislo2)}");
