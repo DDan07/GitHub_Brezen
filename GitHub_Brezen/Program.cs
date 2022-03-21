9125fa5d39fc90686ddff841cccfa5620636499d
Daniel Duda <daniel.duda@educhem.cz>
2022-03-21 11:31:42 +0100

    Doplnění zadání čísla

--- GitHub_Brezen/Program.cs
+++ GitHub_Brezen/Program.cs
@@ -1,4 +1,8 @@
-﻿int cislo1 = Convert.ToInt32(Console.ReadLine());
+﻿Console.WriteLine("Zadej první číslo: ");
+int cislo1 = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine("Zadej druhé číslo: ");
 int cislo2 = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
 
 Console.WriteLine($"{Math.Max(cislo1, cislo2)}, {Math.Min(cislo1, cislo2)}");
